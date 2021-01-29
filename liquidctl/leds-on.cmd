4610d7347f04a2841b789792b846ec040ed1db88
Michał Lower <keton@users.noreply.github.com>
2021-01-29 17:43:06 +0100

    Update leds-on.cmd
    support 2 NZXT Ambient boxes

--- liquidctl/leds-on.cmd
+++ liquidctl/leds-on.cmd
@@ -1,20 +1,24 @@
 @echo off
 
-set COLOR=00d0ff
-
 REM NZXT ambient hue 2 kit
-set DEVNAME=ambient
-
-liquidctl --match %DEVNAME% initialize
+set COLOR=00d0ff
+set SERIALS=6D86435A545,87913F68495
 
 REM all LEDs to single color
-liquidctl --match %DEVNAME% set sync color fixed %COLOR%
+FOR %%S IN (%SERIALS%) DO (
+
+REM do we need to initialize?
+REM liquidctl --serial %%S initialize
+
+liquidctl --serial %%S set sync color fixed %COLOR%
+
+)
 
 REM Gigabyte motherboard lighting
-set DEVNAME=gigabyte
 set COLOR=0000ff
+set PRODUCT=5702
 
-liquidctl --match %DEVNAME% initialize
+liquidctl --product %PRODUCT% initialize
 
 REM only LED1
-liquidctl --match %DEVNAME% set led1 color fixed %COLOR%
+liquidctl --product %PRODUCT% set led1 color fixed %COLOR%
